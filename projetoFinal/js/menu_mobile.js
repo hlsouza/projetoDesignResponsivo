99698211f864ed5cfa070abf8d975d5470dcf7da
hlsouza <45947585+hlsouza@users.noreply.github.com>
2019-05-07 20:57:56 -0300

    Add files via upload

---
+++ projetoFinal/js/menu_mobile.js
@@ -0,0 +1,5 @@
+function mostraMenuMobile() {
+    //alert("Executou mostraMenuMobile()!!");
+    var element = document.getElementById("dropdown-content-mobile01");
+    element.classList.toggle("menu-active");
+}
